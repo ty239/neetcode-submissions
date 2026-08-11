5576bf70da3bb703c36c4e09fa5a2341c5f31673
ty239 <khawajat016@gmail.com>
2026-08-10 21:33:19 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+        let toal = s.split("");
+        let compare = toal.length - 1;
+
+        for (let i = 0; i < toal.length ; i++) {
+            if (toal[i] !== toal[compare]) {
+                return false;
+            }
+
+            compare--;
+        }
+
+        return true;
+    }
+}
